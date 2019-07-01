7431af133ade5dd07fde6074e664a4fc891f4447
sawinw <sawinw.colombo@gmail.com>
2019-07-01 22:18:00 +0530

    modified

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Name\t: %s\n", "Alexandra Abramov");
+    printf("DOB\t: %s\n", "July 14, 1975");
+    printf("Mobile\t: %s\n", "99-9999999999");
+    return 0;
+}
 
